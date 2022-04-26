9dbf9db2a53755446c8370b822f3fbb830016c25
Stepakrap <stepa.krap@gmail.com>
2022-04-26 15:32:27 +0300

    loaded lab

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/marso/Desktop/DevTool/devtools-course-practice
+# Build directory: C:/Users/marso/Desktop/DevTool/devtools-course-practice/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("3rdparty/gtest")
+subdirs("modules/complex-number")
