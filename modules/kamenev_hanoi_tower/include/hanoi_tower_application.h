02b37312ee6eac0abf35d11f68b8fa0191a361ff
Stepakrap <stepa.krap@gmail.com>
2022-06-05 20:56:56 +0300

    fix style again

--- modules/kamenev_hanoi_tower/include/hanoi_tower_application.h
+++ modules/kamenev_hanoi_tower/include/hanoi_tower_application.h
@@ -1,28 +1,28 @@
 // Copyright 2022 Krapivenskikh Stepan
 
-#ifndef MODULES_KRAPIVENSKIKH_S_HANOI_TOWER_INCLUDE_HANOI_TOWER_APPLICATION_H_
-#define MODULES_KRAPIVENSKIKH_S_HANOI_TOWER_INCLUDE_HANOI_TOWER_APPLICATION_H_
+#ifndef MODULES_KAMENEV_HANOI_TOWER_INCLUDE_HANOI_TOWER_APPLICATION_H_
+#define MODULES_KAMENEV_HANOI_TOWER_INCLUDE_HANOI_TOWER_APPLICATION_H_
 
 #include <string>
 #include <vector>
 
 enum class ConsoleOperation {
-	solve_hanoi,
-	solve_hanoi_with_arg,
-	print_all_instructions
+  solve_hanoi,
+  solve_hanoi_with_arg,
+  print_all_instructions
 };
 
 class HanoiTowerApp {
-public:
-	HanoiTowerApp();
+ public:
+  HanoiTowerApp();
 
-	std::string operator()(int argc, const char** argv);
+  std::string operator()(int argc, const char** argv);
 
-private:
-	void help(const char* appname, const char* message = "");
-	bool validateArguments(int argc, const char** argv);
+ private:
+  void help(const char* appname, const char* message = "");
+  bool validateArguments(int argc, const char** argv);
 
-	std::string m_message;
+  std::string m_message;
 };
 
-#endif  // MODULES_KRAPIVENSKIKH_S_HANOI_TOWER_INCLUDE_HANOI_TOWER_APPLICATION_H_
+#endif  // MODULES_KAMENEV_HANOI_TOWER_INCLUDE_HANOI_TOWER_APPLICATION_H_
